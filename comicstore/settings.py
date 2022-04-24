d4d57d8b6e0dcafa6178110ed7450ef66024cd70
Oorestes <orestes06.93@gmail.com>
2022-04-24 16:51:59 -0500

    Finalizadas parte 1 y 2 del examen tecnico

--- comicstore/settings.py
+++ comicstore/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #apps
     'api',
+    #librerias,
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
